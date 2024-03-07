fc7e6f9b4fa239a5df8f3462e40f709afa87a4e1
Ajeesh <ajeeshperalayil@gmail.com>
2024-03-08 02:07:54 +0530

    supplier balance screen

--- lib/dashboard_components/supplier_balance.dart
+++ lib/dashboard_components/supplier_balance.dart
@@ -1,19 +1,182 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:multi_store_app2/widgets/appbar_widgets.dart';
+import 'package:multi_store_app2/widgets/yellow_button.dart';
 
 class BalanceScreen extends StatelessWidget {
   const BalanceScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        title: const AppBarTitle(title: 'BalanceScreen'),
-        centerTitle: true,
-        leading: const AppBarBackButton(),
-      ),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance
+          .collection("orders")
+          .where('sid', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Material(
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        double totalPrice = 0.0;
+        for (var item in snapshot.data!.docs) {
+          totalPrice += item['orderqty'] * item['orderPrice'];
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            title: const AppBarTitle(title: 'Statics'),
+            leading: const AppBarBackButton(),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BalanceModel(
+                  label: "total balance",
+                  value: totalPrice,
+                  decimal: 2,
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  height: 45,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: MaterialButton(
+                    onPressed: () {},
+                    child: const Center(
+                      child: Text(
+                        "Get My Money !",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BalanceModel extends StatelessWidget {
+  final String label;
+  final int decimal;
+  final dynamic value;
+  const BalanceModel({
+    super.key,
+    required this.decimal,
+    required this.label,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 60,
+          width: MediaQuery.of(context).size.width * 0.55,
+          decoration: const BoxDecoration(
+            color: Colors.blueGrey,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25),
+              topRight: Radius.circular(25),
+            ),
+          ),
+          child: Center(
+            child: Text(
+              label.toUpperCase(),
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ),
+        Container(
+          height: 90,
+          width: MediaQuery.of(context).size.width * 0.70,
+          decoration: BoxDecoration(
+            color: Colors.blueGrey.shade100,
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(15),
+            ),
+          ),
+          child: AnimatedCounter(
+            count: value,
+            decimal: decimal,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class AnimatedCounter extends StatefulWidget {
+  final dynamic count;
+  final int decimal;
+  const AnimatedCounter(
+      {super.key, required this.count, required this.decimal});
+
+  @override
+  State<AnimatedCounter> createState() => _AnimatedCounterState();
+}
+
+class _AnimatedCounterState extends State<AnimatedCounter>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _animation;
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    _animation = _controller;
+    setState(() {
+      _animation = Tween(begin: _animation.value, end: widget.count)
+          .animate(_controller);
+    });
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (BuildContext context, Widget? child) {
+        return Center(
+          child: Text(
+            _animation.value.toStringAsFixed(widget.decimal),
+            style: const TextStyle(
+              color: Colors.pink,
+              letterSpacing: 2,
+              fontSize: 40,
+              fontFamily: 'Acme',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      },
     );
   }
 }
